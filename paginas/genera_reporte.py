3e9a710367cfc65234354858cf153b09185402f8
Michoricardo <ricardog94@hotmail.com>
2020-05-18 07:29:01 -0500

    Funciona el primer ciclo con reporte simple txt, resumen txt y resumen feo en pdf. Esto en mailAuth automatizado solamente

--- paginas/genera_reporte.py
+++ paginas/genera_reporte.py
@@ -11,7 +11,7 @@ from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.common.exceptions import ElementNotInteractableException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
-
+from reportlab.pdfgen import canvas
 class reportGenerator:
     def __init__(self,driver):
         self.driver=driver
@@ -39,12 +39,46 @@ class reportGenerator:
                     noElemento = noElemento+1
                 elif 'plataforma' in line:
                     selenium_platform =selenium_platform+1
-            print("Las exitosas fueron: ", exitosas)
+            """print("Las exitosas fueron: ", exitosas)
             print("Se interrumpieron :", interrupcion)
             print("No se pudo hacer click" ,clickInterrumpido)
             print("Se agotó el tiempo de espera: ",timeout)
             print("No se encontró al elemento: ",noElemento)
-            print("Otros errores: ", selenium_platform)
+            print("Otros errores: ", selenium_platform)"""
+        with open ("mailAuthResumen.txt", "a") as f:
+            print("Se genera reporte el: ",time.asctime(),file = f)
+            print('\n', file = f)
+            print(23*"=",file = f)
+            print('\n', file = f)
+            print("Las exitosas fueron: ",exitosas, file = f)
+            print('\n', file = f)
+            print("Se interrumpieron :",interrupcion,file = f)
+            print('\n', file = f)
+            print("No se pudo hacer click" ,clickInterrumpido, file = f)
+            print('\n', file = f)
+            print("Se agotó el tiempo de espera: ",timeout, file = f)
+            print('\n', file = f)
+            print("No se encontró al elemento: ",noElemento, file = f)
+            print('\n', file = f)
+            print("Otros errores: ",selenium_platform, file = f)
+            print('\n', file = f)
+        time.sleep(5)
+        with open('mailAuthResumen.txt') as fobj:
+            lecturaResumen = fobj.readlines()
+            fobj.close()
+            c= canvas.Canvas("Reporte1")
+            from reportlab.lib import colors
+            text = c.beginText(40, 680)
+            text.setFont("Courier", 18)
+            text.setFillColor(colors.blue)
+            for line in lecturaResumen:
+                line.split('\n')
+                text.textLine(line)
+
+            c.drawText(text)            
+            #c.setFont('Helvetica',12)
+            #c.drawString(0,200,lecturaResumen)
+            c.save()
     def tearDown(self):
         driver.quit()
   
