fdb6424cff245f21930fd07f3ceb32ed229d1e13
Michoricardo <ricardog94@hotmail.com>
2020-07-10 18:08:06 -0500

    Modificación de admin orders all & agregar institucional

---
+++ Automatic_Testing/compra_Ramos_INSTITUCIONAL.py
@@ -0,0 +1,134 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import TimeoutException
+import os
+
+class shoppingRamosInstitucional(unittest.TestCase):
+    """def __init__(self,driver):
+        
+        self.driver=driver"""
+    def setUp(self):
+        global driver
+        driver = webdriver.Firefox(executable_path=r"C:\Users\ricar\AppData\Local\Programs\Python\Python38\geckodriver.exe")
+        driver.get("https://gobstore-qa.firebaseapp.com/")
+        esperaBoton = WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,"/html/body/header/div/div/section/button[2]")))
+        print("Haciendo click en iniciar sesion GOBSTORE")
+        print("<br>")
+        esperaBoton.click()
+        driver.implicitly_wait(3)
+        #Mail AUTH
+        mailAuth = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="emailLoginForm"]')))
+        if mailAuth is not None:
+            print("Campo para autenticacion por mail encontrado,escribiendo email")
+            print("<br>")
+            mailAuth.send_keys("micho@gobstore.mx")
+            mailpwd = driver.find_element_by_xpath('//*[@id="pwdLoginForm"]')
+        if mailpwd is not None:
+            print("Se encontro el campo para password, enviando password...")
+            print("<br>")
+            mailpwd.send_keys("C0business.")
+        boton_ingresar = driver.find_element_by_xpath('//*[@id="btnLoginForm"]')
+        if boton_ingresar is not None:
+            print("Se encontro el boton ingresar, haciendo click")
+            print("<br>")
+            boton_ingresar.click()
+            print("Inicio de sesion exitoso")
+            print("<br>")
+            time.sleep(4)
+
+    def test_compraChicharron(self):
+        driver.get("https://gobstore-qa.firebaseapp.com/carnesramos")
+        global codigoPostal
+        codigoPostal= "64849"
+        time.sleep(3)
+        try:
+            modal = WebDriverWait(driver,40).until(EC.presence_of_element_located((By.XPATH,'//*[@id="postalCodeModal"]/div/div')))
+            if modal is not None:
+                blank_cp = driver.find_element_by_id("inputPostalCode")
+                if blank_cp is not None:
+                    print("Agregando CP")
+                    print("<br>")
+                    blank_cp.send_keys(codigoPostal)
+                    vertienda = driver.find_element_by_id("btnGetOpByPostalCode")
+                    vertienda.click()
+                    time.sleep(4)
+        except ElementNotInteractableException as exception:
+            print("No tenias que poner CP porque ya habías comprado :)")
+            print("<br>")
+        time.sleep(3)
+        btn_chicharron = driver.find_element_by_xpath('//*[@id="customSectionsWrapper"]/div/div[2]/div[2]/div[1]')
+        if btn_chicharron is not None:
+            print("Clickeando imagen de chicharron para agregar a cart")
+            print("<br>")
+            btn_chicharron.click()
+            driver.implicitly_wait(10)
+            time.sleep(2)    
+        btn_addToCart = driver.find_element_by_id("btnAddItemToCard")
+        if btn_addToCart is not None:
+            print("Haciendo click en Agregar al carrito/ QTY default (1)")
+            print("<br>")
+            btn_addToCart.click()
+            driver.implicitly_wait(10)
+            time.sleep(2)
+        cambio_pagina =driver.get("https://gobstore-qa.firebaseapp.com/carnesramos/checkout")
+        print("Llendo a checkout")
+        print("<br>")
+        time.sleep(5)
+        #botón de institucional
+        btn_INST = driver.find_element_by_xpath('//*[@id="b2bPaymentLi"]/a')
+        if btn_INST is not None:
+            print("Eligiendo metodo de pago Institucional o B2B")
+            print("<br>")
+            btn_INST.click()
+            driver.implicitly_wait(10)
+            time.sleep(8)
+        driver.execute_script("window.scrollTo(0,400)")
+        time.sleep(3)
+        driver.execute_script("window.scrollTo(0,1200)")
+        datepicker = driver.find_element_by_xpath('//*[@id="inputDate"]')
+        if datepicker is not None:
+            print("Date picker encontrado")
+            print("<br>")
+            time.sleep(2)
+            actions= ActionChains(driver)
+            actions.move_to_element(datepicker).click().perform()
+            print("Eligiendo fecha default")
+            print("<br>")
+        falto_algo=driver.find_element_by_xpath('//*[@id="extraComments"]').send_keys("No faltó nada")
+        time.sleep(1)
+        ordenar_btn=driver.find_element_by_xpath('//*[@id="handlePlaceOrder"]')
+        if ordenar_btn is not None:
+            print("Ordenando ...")
+            print("<br>")
+            ordenar_btn.click()
+            time.sleep(20)
+        urlcompra = driver.current_url
+        print("llego aqui")
+        if urlcompra is not None:
+            if "thankyou?order=" in urlcompra:
+                print("Enlace de compra:")
+                print("<br>")
+                print(urlcompra)
+                print("<br>")
+                #<a href="quien-soy.html">Quién soy</a>
+                print("<a href=")
+                print(urlcompra)
+                print(">")
+                print(urlcompra)
+                print("</a>")
+                #print('<a href="','"',urlcompra,'"'>urlcompra,'</a>')
+            
+    def tearDown(self):
+        driver.quit()
+
+if __name__ == "__main__":
+   unittest.main()
